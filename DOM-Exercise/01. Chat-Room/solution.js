5ff0874cc9917904cb298c29f9d5271cd0ac7523
Vasil <vvpanayotovv@gmail.com>
2020-12-08 20:05:05 +0200

    first solution implemented

--- DOM-Exercise/01. Chat-Room/solution.js
+++ DOM-Exercise/01. Chat-Room/solution.js
@@ -1,5 +1,20 @@
 function solve() {
-   //TODO...
+
+   const sendBtn = document.getElementById('send')
+   const textarea = document.getElementById('chat_input')
+
+   sendBtn.addEventListener('click', sendMassage)
+
+   function sendMassage(){
+   
+      let massage = textarea.value;
+      let newMassage = document.createElement('div');
+      newMassage.classList.add('message','my-message');
+      newMassage.textContent = massage;
+
+      document.getElementById('chat_messages').appendChild(newMassage);
+      console.log(textarea)
+
+      textarea.value = '';
+   }
 }
-
-
