4ae50081a217073377ed824fe86a6f23d554053e
Vasil <vvpanayotovv@gmail.com>
2020-12-08 21:32:51 +0200

    implemented second problem

--- DOM-Exercise/02. Number-Convertor/solution.js
+++ DOM-Exercise/02. Number-Convertor/solution.js
@@ -1,4 +1,53 @@
 function solve() {
 
-    //TODO...
+    const selectMenuTo = document.getElementById('selectMenuTo');
+
+    document.querySelector("#container > button").addEventListener('click', convert);
+
+
+    function convert() {
+
+        let number = Number(document.getElementById('input').value);
+
+        let result;
+
+        if (selectMenuTo.value === 'binary') {
+            result = convertToBinary(number);
+        } else if (selectMenuTo.value === 'hexadeicmal') {
+            result = convertToHexadeicmal(number);
+        }
+
+        appendResult(result);
+
+    }
+
+    function appendResult(result){
+        document.getElementById('result').value = result;
+    }
+
+    function convertToBinary(number){
+        return (number >>> 0).toString(2);
+    }
+
+    function convertToHexadeicmal(number){
+        return number.toString(16);
+    }
+
+    function createSelectMenuOption() {
+        let binaryOption = document.createElement('option');
+        binaryOption.textContent = 'Binary';
+        binaryOption.value = 'binary';
+
+
+        let hexadeicmalOption = document.createElement('option');
+        hexadeicmalOption.textContent = 'Hexadeicmal';
+        hexadeicmalOption.value = 'hexadeicmal';
+
+        selectMenuTo.appendChild(binaryOption);
+        selectMenuTo.appendChild(hexadeicmalOption);
+
+    }
+
+    createSelectMenuOption();
+
 }
