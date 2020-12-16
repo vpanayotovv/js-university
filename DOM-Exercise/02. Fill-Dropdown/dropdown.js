f3df994e364b2f9376d2ceb458c827eb0e74858f
Vasil <vvpanayotovv@gmail.com>
2020-12-16 17:59:25 +0200

    dropdown

---
+++ DOM-Exercise/02. Fill-Dropdown/dropdown.js
@@ -0,0 +1,14 @@
+function addItem() {
+    let text = document.getElementById('newItemText');
+    let data = document.getElementById('newItemValue');
+
+    let select = document.getElementById('menu');
+    let option = document.createElement('option');
+    option.value = data.value;
+    option.innerHTML = text.value;
+
+    select.appendChild(option);
+
+    text.value = '';
+    data.value = '';
+}
