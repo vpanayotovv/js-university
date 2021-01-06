7b1019aefff9dd4835a7c0240b83a3f5edb492bf
Vasil <vvpanayotovv@gmail.com>
2021-01-06 13:07:40 +0200

    deleted

--- DOM-Manipulation-Lab/02. Add-Delete/app.js
+++ DOM-Manipulation-Lab/02. Add-Delete/app.js
@@ -5,7 +5,7 @@ function addItem() {
 
     let liElement = document.createElement('li');
     let spanElement = document.createElement('span');
-    spanElement.innerText = '[Delete]'
+    spanElement.innerText = ' [Delete]'
     spanElement.addEventListener('click',function deleteElement(event) {
         let parentElement = event.target.parentElement;
         parentElement.remove();
