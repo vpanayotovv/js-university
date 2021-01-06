55e81f5929450d8a2712f6f2b9f6555b00556b47
Vasil <vvpanayotovv@gmail.com>
2021-01-06 13:03:25 +0200

    Revert "up"
    This reverts commit 28aaf18845d9481ed9e25d2ff025dffb2018a373.

--- DOM-Manipulation-Lab/02. Add-Delete/app.js
+++ DOM-Manipulation-Lab/02. Add-Delete/app.js
@@ -5,11 +5,7 @@ function addItem() {
 
     let liElement = document.createElement('li');
     let spanElement = document.createElement('span');
-
-    spanElement.innerText = ' [Delete]';
-    spanElement.style.cursor = 'pointer';
-    spanElement.style.color = 'red';
-
+    spanElement.innerText = '[Delete]'
     spanElement.addEventListener('click',function deleteElement(event) {
         let parentElement = event.target.parentElement;
         parentElement.remove();
