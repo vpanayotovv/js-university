7808411253c6b87099d00e50512e6b6c477b0cdf
Vasil <vvpanayotovv@gmail.com>
2021-01-06 14:34:06 +0200

    solved

--- DOM-Manipulation-Lab/03. Delete-From-Table/app.js
+++ DOM-Manipulation-Lab/03. Delete-From-Table/app.js
@@ -1,3 +1,23 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+
+    let inputElement = document.querySelector("body > label > input[type=text]");
+    let inputValue = inputElement.value;
+
+    let customerEmails = document.querySelectorAll('#customers tbody td:nth-of-type(2)');
+    let isDeleted = false;
+
+    for (const customer of customerEmails) {
+        if(customer.textContent === inputValue){
+            customer.parentElement.remove();
+            isDeleted = true;
+        }
+    }
+
+    if(!isDeleted){
+        let error = document.getElementById('result');
+        error.innerHTML = 'Not found';
+    }else{
+        let error = document.getElementById('result');
+        error.innerHTML = 'Deleted';
+    }
 }
