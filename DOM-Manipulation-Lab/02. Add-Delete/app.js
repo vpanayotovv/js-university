13fbf8599f3bebb64f7cb68d10096b833a187b4a
Vasil <vvpanayotovv@gmail.com>
2021-01-06 12:51:05 +0200

    solved

---
+++ DOM-Manipulation-Lab/02. Add-Delete/app.js
@@ -0,0 +1,21 @@
+function addItem() {
+
+    let ulElement = document.getElementById('items');
+    let inputText = document.getElementById('newText');
+
+    let liElement = document.createElement('li');
+    let spanElement = document.createElement('span');
+    spanElement.innerText = '[Delete]'
+    spanElement.addEventListener('click',function deleteElement(event) {
+        let parentElement = event.target.parentElement;
+        parentElement.remove();
+    })
+
+
+    liElement.innerHTML = inputText.value;
+    
+    ulElement.appendChild(liElement);
+    liElement.appendChild(spanElement);
+    
+    inputText.value = '';
+}
