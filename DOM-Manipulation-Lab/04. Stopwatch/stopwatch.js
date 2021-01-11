b8fcfed2bb4a041a26cabddbe413727147ecad25
Vasil <vvpanayotovv@gmail.com>
2021-01-11 10:23:58 +0200

    implemented

--- DOM-Manipulation-Lab/04. Stopwatch/stopwatch.js
+++ DOM-Manipulation-Lab/04. Stopwatch/stopwatch.js
@@ -1,3 +1,35 @@
 function stopwatch() {
-    console.log('TODO:...');
+    let timerElement = document.getElementById('time');
+    let startButtonElement = document.getElementById('startBtn');
+    let stopButtonElement = document.getElementById('stopBtn');
+    let interval;
+
+    startButtonElement.addEventListener('click', function (e) {
+        timerElement.textContent = '00:00';
+        stopButtonElement.removeAttribute('disabled');
+        e.currentTarget.setAttribute('disabled',true);
+
+        interval = setInterval(function () {
+
+            let currentTime = timerElement.textContent;
+            let timeArray = currentTime.split(':')
+            let minutes = Number(timeArray[0]);
+            let seconds = Number(timeArray[1]);
+
+            seconds++;
+            if (seconds > 59) {
+                minutes++;
+                seconds = 0;
+            }
+
+            timerElement.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+        }, 1000);
+
+    });
+
+    stopButtonElement.addEventListener('click', function () {
+        clearInterval(interval);
+        startButtonElement.removeAttribute('disabled');
+        stopButtonElement.setAttribute('disabled',true);
+    })
 }
