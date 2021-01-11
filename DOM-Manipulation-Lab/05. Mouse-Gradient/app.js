a3035ec4f3e7fa4858ae3d73a53672f974983f94
Vasil <vvpanayotovv@gmail.com>
2021-01-11 10:42:52 +0200

    implemented

--- DOM-Manipulation-Lab/05. Mouse-Gradient/app.js
+++ DOM-Manipulation-Lab/05. Mouse-Gradient/app.js
@@ -1,3 +1,13 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradientElement = document.getElementById('gradient');
+
+    gradientElement.addEventListener('click', function (e) {
+        let offset = e.offsetX;
+        let width = e.currentTarget.offsetWidth;
+        let procent = (offset / width) * 100;
+
+        let resultElement = document.getElementById('result');
+
+        resultElement.textContent = `${procent.toFixed(0)}%`;
+    })
 }
