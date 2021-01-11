b549d224195de0b69f72867296aa6fa3f30b25fd
Vasil <vvpanayotovv@gmail.com>
2021-01-11 10:56:56 +0200

    implemented

--- DOM-Manipulation-Lab/06. Highlight-Active/app.js
+++ DOM-Manipulation-Lab/06. Highlight-Active/app.js
@@ -1,3 +1,18 @@
 function focus() {
-    console.log('TODO:...');
+
+    let inputs = document.querySelectorAll('input[type="text"');
+
+    Array.from(inputs).forEach(x => {
+        x.addEventListener('focus', function (e) {
+            let parentDivElement = e.currentTarget.parentElement;
+            parentDivElement.classList.add('focused');
+        });
+
+        x.addEventListener('blur', function (e) {
+            let parentDivElement = e.currentTarget.parentElement;
+            parentDivElement.classList.remove('focused'); 
+        });
+    })
+
+
 }
