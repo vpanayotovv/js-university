2e1111a851b1aaf7fe6c411ba9621468be4e262c
Vasil <vvpanayotovv@gmail.com>
2021-01-11 11:07:06 +0200

    implemented

--- DOM-Manipulation-Lab/07. Dynamic-Validation/app.js
+++ DOM-Manipulation-Lab/07. Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    console.log('TODO:...');
+    let inputElement = document.getElementById('email');
+    inputElement.addEventListener('change', function (e) {
+        let currentValue = e.currentTarget.value;
+        let pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        
+        if (pattern.test(currentValue)) {
+            e.currentTarget.classList.remove('error');
+
+        } else {
+            e.currentTarget.classList.add('error');
+        
+        }
+    })
 }
